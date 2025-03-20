57646aa660dc9440abc39db179bedab528b36da1
delliaptr <sdelliaputri@gmail.com>
2025-03-20 17:12:26 +0700

    Add files via upload

--- src/scripts/data/api.js
+++ src/scripts/data/api.js
@@ -1,10 +1,38 @@
 import CONFIG from '../config';
 
 const ENDPOINTS = {
-  ENDPOINT: `${CONFIG.BASE_URL}/your/endpoint/here`,
+  STORIES: `${CONFIG.BASE_URL}/stories`,
+  STORY_DETAIL: (id) => `${CONFIG.BASE_URL}/stories/${id}`,
+  ADD_STORY: `${CONFIG.BASE_URL}/stories`,
 };
 
-export async function getData() {
-  const fetchResponse = await fetch(ENDPOINTS.ENDPOINT);
-  return await fetchResponse.json();
+class Api {
+  static async getStories() {
+    const response = await fetch(ENDPOINTS.STORIES);
+    const responseJson = await response.json();
+    return responseJson.data.stories;
+  }
+
+  static async getStoryDetail(id) {
+    const response = await fetch(ENDPOINTS.STORY_DETAIL(id));
+    const responseJson = await response.json();
+    return responseJson.data.story;
+  }
+
+  static async addStory(formData) {
+    const response = await fetch(ENDPOINTS.ADD_STORY, {
+      method: 'POST',
+      body: formData,
+    });
+    const responseJson = await response.json();
+    return responseJson;
+  }
+
+  static async getStoriesWithLocation() {
+    const response = await fetch(`${ENDPOINTS.STORIES}?location=1`);
+    const responseJson = await response.json();
+    return responseJson.data.stories;
+  }
 }
+
+export default Api;
