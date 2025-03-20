4f1ce2a1a163c4fba2eca6577d5384018706a077
delliaptr <sdelliaputri@gmail.com>
2025-03-20 17:20:00 +0700

    Add files via upload

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,9 +1,15 @@
 import HomePage from '../pages/home/home-page';
 import AboutPage from '../pages/about/about-page';
+import DetailPage from '../pages/detail/detail-page';
+import AddStoryPage from '../pages/add/add-story-page';
+import MapPage from '../pages/map/map-page';
 
 const routes = {
   '/': new HomePage(),
   '/about': new AboutPage(),
+  '/detail/:id': new DetailPage(),
+  '/add': new AddStoryPage(),
+  '/map': new MapPage(),
 };
 
 export default routes;
