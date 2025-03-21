81543cf68ad605d7f65bbf094607bbfb19245b83
delliaptr <sdelliaputri@gmail.com>
2025-03-21 10:21:58 +0700

    Add files via upload

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -3,6 +3,8 @@ import AboutPage from '../pages/about/about-page';
 import DetailPage from '../pages/detail/detail-page';
 import AddStoryPage from '../pages/add/add-story-page';
 import MapPage from '../pages/map/map-page';
+import LoginPage from '../pages/auth/login-page';
+import RegisterPage from '../pages/auth/register-page';
 
 const routes = {
   '/': new HomePage(),
@@ -10,6 +12,8 @@ const routes = {
   '/detail/:id': new DetailPage(),
   '/add': new AddStoryPage(),
   '/map': new MapPage(),
+  '/login': new LoginPage(),
+  '/register': new RegisterPage(),
 };
 
 export default routes;
